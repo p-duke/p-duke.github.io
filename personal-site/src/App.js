e346cd9fd52b2f17775c04d01de9ada5bec92f1b
p-duke <peteraaronduke@gmail.com>
2016-10-02 15:01:15 -0400

    created modals in portfolio section

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -52,31 +52,112 @@ class App extends Component {
           </Grid>
         </Jumbotron>
 
-        <div className="container-fluid">
+
+        {/* Carousel Section */}
+        {/* <div className="container-fluid">
           <Carousel>
             <Carousel.Item>
               <img className="img-responsive center-block" width={900} height={500} alt="900x500" src={require('../images/Homepage.png')}/>
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <Carousel.Caption className="carousel-text">
+                <div className="overlay">
+                  <h3>Homepage</h3>
+                  <p>See what's trending including current top mentors.</p>
+                </div>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img className="img-responsive center-block" width={900} height={500} alt="900x500" src={require('../images/mentor_search.png')}/>
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <Carousel.Caption className="carousel-text">
+                <div className="overlay">
+                  <h3>Mentor Search</h3>
+                  <p>Search for mentors that have skills you wish to learn.</p>
+                </div>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img className="img-responsive center-block" width={900} height={500} alt="900x500" src={require('../images/user-profile.png')}/>
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              <Carousel.Caption className="carousel-text">
+                <div className="overlay">
+                  <h3>Profile Page</h3>
+                  <p>Create and Edit your profile including skills, bio, and goals.</p>
+                </div>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
-        </div>
+        </div> */}
+
+        <section id="portfolio">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 text-center">
+                        <h2>Portfolio</h2>
+                        <hr className="star-primary" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-4 portfolio-item">
+                        <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                            <div className="caption">
+                                <div className="caption-content">
+                                    <i className="fa fa-search-plus fa-3x"></i>
+                                </div>
+                            </div>
+                            <img src={require('../images/cabin.png')} className="img-responsive" alt="" />
+                        </a>
+                    </div>
+                    <div className="col-sm-4 portfolio-item">
+                        <a href="#portfolioModal2" className="portfolio-link" data-toggle="modal">
+                            <div className="caption">
+                                <div className="caption-content">
+                                    <i className="fa fa-search-plus fa-3x"></i>
+                                </div>
+                            </div>
+                            <img src={require('../images/cake.png')} className="img-responsive" alt="" />
+                        </a>
+                    </div>
+                    <div className="col-sm-4 portfolio-item">
+                        <a href="#portfolioModal3" className="portfolio-link" data-toggle="modal">
+                            <div className="caption">
+                                <div className="caption-content">
+                                    <i className="fa fa-search-plus fa-3x"></i>
+                                </div>
+                            </div>
+                            <img src={require('../images/circus.png')} className="img-responsive" alt="" />
+                        </a>
+                    </div>
+                    <div className="col-sm-4 portfolio-item">
+                        <a href="#portfolioModal4" className="portfolio-link" data-toggle="modal">
+                            <div className="caption">
+                                <div className="caption-content">
+                                    <i className="fa fa-search-plus fa-3x"></i>
+                                </div>
+                            </div>
+                            <img src={require('../images/game.png')} className="img-responsive" alt="" />
+                        </a>
+                    </div>
+                    <div className="col-sm-4 portfolio-item">
+                        <a href="#portfolioModal5" className="portfolio-link" data-toggle="modal">
+                            <div className="caption">
+                                <div className="caption-content">
+                                    <i className="fa fa-search-plus fa-3x"></i>
+                                </div>
+                            </div>
+                            <img src={require('../images/safe.png')} className="img-responsive" alt="" />
+                        </a>
+                    </div>
+                    <div className="col-sm-4 portfolio-item">
+                        <a href="#portfolioModal6" className="portfolio-link" data-toggle="modal">
+                            <div className="caption">
+                                <div className="caption-content">
+                                    <i className="fa fa-search-plus fa-3x"></i>
+                                </div>
+                            </div>
+                            <img src={require('../images/submarine.png')} className="img-responsive" alt="" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
 
         <Grid fluid bsClass="container" className="bg-2">
           <h1>Resume</h1>
