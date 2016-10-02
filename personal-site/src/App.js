441658b3dc65ab6b2ce10745592b924612fd1549
p-duke <peteraaronduke@gmail.com>
2016-10-01 21:08:59 -0400

    navigation finished and started header

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import './index.css';
+import profilePic from '../images/profile.png';
+import {
+  Grid,
+  Navbar,
+  Jumbotron,
+  Button,
+  NavItem,
+  Nav,
+  NavDropdown,
+  MenuItem,
+  Image,
+  Row
+} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar inverse fixedTop>
+      <Navbar inverse fixedTop>
           <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">React App</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Peter Duke</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+          </Nav>
+          </Navbar.Collapse>
           </Grid>
         </Navbar>
-        <Jumbotron>
+        {/* <Jumbotron>
           <Grid>
             <h1>Welcome to React</h1>
             <p>
@@ -30,7 +50,17 @@ class App extends Component {
               </Button>
             </p>
           </Grid>
-        </Jumbotron>
+        </Jumbotron> */}
+
+        {/* Header Section */}
+        <Grid fluid bsClass='container' className="container-fluid bg-1 text-center wrapper">
+            <h3 className="margin">Who Am I?</h3>
+            <Image rounded responsive src={profilePic} className="center-block" alt="profile-pic" />
+            <h3>I'm an adventurer</h3>
+        </Grid>
+
+
+
       </div>
     );
   }
