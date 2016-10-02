0d992fd5e7aaa868cdb8de8e2c9b6b3a0eb31149
p-duke <peteraaronduke@gmail.com>
2016-10-01 23:45:13 -0400

    added parallax feature plus background image

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -20,47 +20,38 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Navbar inverse fixedTop>
+      <Navbar inverse fixedTop className="navbar">
           <Grid>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Peter Duke</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-          <Nav pullRight>
-            <NavItem eventKey={1} href="#">Link</NavItem>
-            <NavItem eventKey={2} href="#">Link</NavItem>
-            <NavItem eventKey={2} href="#">Link</NavItem>
-          </Nav>
-          </Navbar.Collapse>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#">Peter Duke</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">Link</NavItem>
+              <NavItem eventKey={2} href="#">Link</NavItem>
+              <NavItem eventKey={2} href="#">Link</NavItem>
+            </Nav>
+            </Navbar.Collapse>
           </Grid>
         </Navbar>
-        {/* <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron> */}
-
         {/* Header Section */}
-        <Grid fluid bsClass='container' className="container-fluid bg-1 text-center wrapper">
-            <h3 className="margin">Who Am I?</h3>
-            <Image rounded responsive src={profilePic} className="center-block" alt="profile-pic" />
-            <h3>I'm an adventurer</h3>
+
+        <Jumbotron>
+          <Grid fluid className="parallax text-center">
+          <div className="text-center">
+            <h1 className="margin title">Peter Duke</h1>
+            {/* <Image rounded responsive src={profilePic} className="center-block" alt="profile-pic" /> */}
+            <h2 className="margin subhead">Web Developer</h2>
+          </div>
+          </Grid>
+          <div className="overlay"></div>
+        </Jumbotron>
+        <Grid fluid bsClass='container' className="text-center">
+          <p>Here's something else</p>
         </Grid>
-
-
-
       </div>
     );
   }
