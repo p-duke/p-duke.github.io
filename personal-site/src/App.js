9946fc9cd78d8bafce537812b114953d160d6e94
p-duke <peteraaronduke@gmail.com>
2016-10-03 12:18:47 -0400

    setup portfolio layout without modal functionality. Edited jumbotron display

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -17,7 +17,8 @@ import {
   MenuItem,
   Image,
   Row,
-  Carousel
+  Carousel,
+  Media
 } from 'react-bootstrap';
 
 class App extends Component {
@@ -37,24 +38,30 @@ class App extends Component {
             </Navbar.Header>
             <Navbar.Collapse>
             <Nav pullRight>
-              <NavItem eventKey={1} href="#">Link</NavItem>
-              <NavItem eventKey={2} href="#">Link</NavItem>
-              <NavItem eventKey={2} href="#">Link</NavItem>
+              <NavItem href="#">Portfolio</NavItem>
+              <NavItem href="#">About</NavItem>
+              <NavItem href="#">Contact</NavItem>
             </Nav>
             </Navbar.Collapse>
           </Grid>
         </Navbar>
 
         {/* Header Section */}
-        <Jumbotron id="hero">
+        <Jumbotron id="hero" className="vertical-center">
           <div id="hero-overlay"></div>
-          <Grid fluid className="text-center">
-          <div className="text-center">
+          <Grid className="text-center">
+          <div id="hero-text" className="text-center">
             <h1 className="margin">Peter Duke</h1>
-            <h2 className="margin">Web Developer</h2>
+            <hr className="thick-line"/>
+            <h2 className="margin">Software Developer by Day</h2>
+            <h3 className="margin">Designer by Night</h3>
           </div>
           </Grid>
         </Jumbotron>
+
+
+
+
 
 
         {/* Carousel Section */}
@@ -90,6 +97,7 @@ class App extends Component {
           </Carousel>
         </div> */}
 
+        {/* Portfolio Section */}
         <section id="portfolio">
             <div className="container">
                 <div className="row">
@@ -98,7 +106,7 @@ class App extends Component {
                         <hr className="star-primary" />
                     </div>
                 </div>
-                <div className="row">
+                {/* <div className="row">
                     <div className="col-md-offset-3 portfolio-item">
                         <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
                             <div className="caption">
@@ -109,62 +117,36 @@ class App extends Component {
                             <img src={require('../images/mentor_search.png')} className="img-responsive" alt="" />
                         </a>
                     </div>
-                    {/* <div className="col-sm-4 portfolio-item">
-                        <a href="#portfolioModal2" className="portfolio-link" data-toggle="modal">
-                            <div className="caption">
-                                <div className="caption-content">
-                                    <i className="fa fa-search-plus fa-3x"></i>
-                                </div>
-                            </div>
-                            <img src={require('../images/cake.png')} className="img-responsive" alt="" />
-                        </a>
-                    </div>
-                    <div className="col-sm-4 portfolio-item">
-                        <a href="#portfolioModal3" className="portfolio-link" data-toggle="modal">
-                            <div className="caption">
-                                <div className="caption-content">
-                                    <i className="fa fa-search-plus fa-3x"></i>
-                                </div>
-                            </div>
-                            <img src={require('../images/circus.png')} className="img-responsive" alt="" />
-                        </a>
-                    </div>
-                    <div className="col-sm-4 portfolio-item">
-                        <a href="#portfolioModal4" className="portfolio-link" data-toggle="modal">
-                            <div className="caption">
-                                <div className="caption-content">
-                                    <i className="fa fa-search-plus fa-3x"></i>
-                                </div>
-                            </div>
-                            <img src={require('../images/game.png')} className="img-responsive" alt="" />
-                        </a>
-                    </div>
-                    <div className="col-sm-4 portfolio-item">
-                        <a href="#portfolioModal5" className="portfolio-link" data-toggle="modal">
-                            <div className="caption">
-                                <div className="caption-content">
-                                    <i className="fa fa-search-plus fa-3x"></i>
-                                </div>
-                            </div>
-                            <img src={require('../images/safe.png')} className="img-responsive" alt="" />
-                        </a>
-                    </div>
-                    <div className="col-sm-4 portfolio-item">
-                        <a href="#portfolioModal6" className="portfolio-link" data-toggle="modal">
-                            <div className="caption">
-                                <div className="caption-content">
-                                    <i className="fa fa-search-plus fa-3x"></i>
-                                </div>
-                            </div>
-                            <img src={require('../images/submarine.png')} className="img-responsive" alt="" />
-                        </a>
-                    </div> */}
+                </div> */}
+
+                <div className="row">
+                <Media className="col-md-4">
+                 <Media.Left align="middle">
+                  <div className="portfolio-item">
+                     <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                         <div className="caption">
+                             <div className="caption-content">
+                                 <i className="fa fa-search-plus fa-3x"></i>
+                             </div>
+                         </div>
+                         <img className="img-responsive" src={require('../images/mentor_search.png')} alt=""/>
+                     </a>
+                     </div>
+                  </Media.Left>
+                  </Media>
+
+                  <Media className="col-md-8">
+                   <Media.Body>
+                    <Media.Heading>Devconnect</Media.Heading>
+                    <p>Selected as team lead for Devconnect, a hub for Dev Bootcamp graduates to connect and flourish in its thriving community plus extending the culture of learning through mentoring. An alumni can contribute to DBC's 2000+ community by posting any relevant information to our extensive forums section. The top trending and most recent forum posts will appear in a tabbed content section on the homepage. If an alumni decides to become a "mentor" they can tutor students who are seeking specific skill sets that the mentor has and later be endorsed for those skills earning badges based on a number of endorsements. The top mentors will appear on the homepage celebrating their achievements in the community. The application was built with Ruby on Rails, Python, Flask, React.js, HTML5, and Bootstrap CSS.</p>
+                  </Media.Body>
+                </Media>
                 </div>
             </div>
         </section>
 
         <Grid fluid bsClass="container" className="bg-2">
-          <h1>Resume</h1>
+          <h1>Contact</h1>
             {/* <Image rounded responsive src={profilePic} className="center-block" alt="profile-pic" /> */}
         </Grid>
 
